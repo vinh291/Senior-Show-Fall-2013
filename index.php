da74ecb6642b388df70214ddae831267b698d8c9
Vinh Le <vinh@istrategylabs.com>
2013-11-14 23:18:34 -0500

    Bold style

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 					</ul>
 					<p>Join us! We’re thrilled to share the culminating work of our college careers with you. Explore the cultural roots of minimalism, wrestle with the hidden world of homelessness, and learn about the nuances of beer and wine. 
 					Enjoy complimentary refreshments, convenient parking, and the opportunity to meet the artists behind the projects.
-					If you can’t make the official reception, stop by beforehand. Our show runs from Monday the 9th through Friday the 13th.
+					If you can’t make the official reception, stop by beforehand. <span class="bold">Our show runs from Monday the 9th through Friday the 13th.</span>
 					Free parking will be available in lots A and K for the closing reception on December 13th. </p>
 
 			</section>
